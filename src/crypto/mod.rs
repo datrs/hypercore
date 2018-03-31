e1c6a29f294d7cc03c6b8556b342d16ccf35c0d7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-31 15:18:27 +0200

    comment out statics

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,9 +1,9 @@
-lazy_static! {
-  static ref LEAF_TYPE: &'static [u8] = b"0";
-  static ref PARENT_TYPE: &'static [u8] = b"1";
-  static ref ROOT_TYPE: &'static [u8] = b"2";
-  static ref HYPERCORE: &'static [u8] = b"hypercore";
-}
+// lazy_static! {
+//   static ref LEAF_TYPE: &'static [u8] = b"0";
+//   static ref PARENT_TYPE: &'static [u8] = b"1";
+//   static ref ROOT_TYPE: &'static [u8] = b"2";
+//   static ref HYPERCORE: &'static [u8] = b"hypercore";
+// }
 
 /// Generate an Ed25519 keypair.
 pub mod key_pair;
