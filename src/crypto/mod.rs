1c7c9f45cbdc3464c42401c57fbf47e681d00259
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-01 16:10:01 +0200

    hash parent method

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -7,6 +7,7 @@ lazy_static! {
 
 extern crate blake2_rfc as blake2;
 extern crate byteorder;
+extern crate merkle_tree_stream as merkle_stream;
 extern crate rust_sodium as sodium;
 
 pub use self::blake2::blake2b::Blake2bResult;
@@ -14,6 +15,7 @@ pub use self::sodium::crypto::sign::ed25519::{PublicKey, SecretKey, Signature};
 
 use self::blake2::blake2b::Blake2b;
 use self::byteorder::{BigEndian, WriteBytesExt};
+use self::merkle_stream::Node;
 use self::sodium::crypto::sign::ed25519::{sign_detached, verify_detached};
 
 /// Generate an `Ed25519` keypair.
@@ -33,16 +35,34 @@ pub fn verify(
   verify_detached(signature, &data, public_key)
 }
 
-/// Compute the hash of a leaf block using `BLAKE2b`.
+/// Compute the hash of a leaf node using `BLAKE2b`.
+/// TODO: accept `Node` types.
 pub fn hash_leaf(data: &[u8]) -> Blake2bResult {
-  let mut writer = vec![];
-  writer
+  let mut size = vec![]; // FIXME: allocate once only.
+  size
     .write_u64::<BigEndian>(data.len() as u64)
     .unwrap();
 
   let mut hasher = Blake2b::new(32);
   hasher.update(*LEAF_TYPE);
-  hasher.update(&writer);
+  hasher.update(&size);
   hasher.update(data);
+  hasher.finalize()
+}
+
+/// Compute the hash of a parent node using `BLAKE2b`.
+pub fn hash_parent(a: &Node, b: &Node) -> Blake2bResult {
+  assert!(b.index > a.index);
+
+  let mut size = vec![]; // FIXME: allocate once only.
+  size
+    .write_u64::<BigEndian>((a.size + b.size) as u64)
+    .unwrap();
+
+  let mut hasher = Blake2b::new(32);
+  hasher.update(*PARENT_TYPE);
+  hasher.update(&size);
+  hasher.update(&a.hash);
+  hasher.update(&b.hash);
   hasher.finalize()
 }
