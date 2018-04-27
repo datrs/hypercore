f6741c37c9d0b333b29790d5451a25f7550d2b8e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-27 15:03:13 +0200

    mv files in src/

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@
 
 #[macro_use]
 extern crate lazy_static;
-
 #[macro_use]
 extern crate failure;
 
@@ -15,6 +14,40 @@ pub mod bitfield;
 pub mod crypto;
 pub mod storage;
 
-mod feed;
+use self::failure::Error;
+use std::path::PathBuf;
 
-pub use feed::*;
+/// Append-only log structure.
+pub struct Feed {
+  /// Location on disk of where to persist the archive.
+  pub path: PathBuf,
+}
+
+impl Feed {
+  /// Create a new instance with an on-disk storage backend.
+  pub fn new(_path: PathBuf) -> Self {
+    unimplemented!();
+  }
+
+  /// Create a new instance with a custom storage backend.
+  pub fn with_storage() -> Self {
+    unimplemented!();
+  }
+
+  /// Append data into the log.
+  pub fn append(&self, _data: &[u8]) -> Result<(), Error> {
+    unimplemented!();
+  }
+
+  /// Retrieve data from the log.
+  pub fn get(&self, _index: usize) -> Option<&[u8]> {
+    unimplemented!();
+  }
+}
+
+/// Create a new instance with an in-memory storage backend.
+impl Default for Feed {
+  fn default() -> Self {
+    unimplemented!();
+  }
+}
