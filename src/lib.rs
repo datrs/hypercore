22a9907bcf67ab8e272488434a88cf74be7696d2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-18 12:43:44 +0200

    re-enable warning failures

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-// #![deny(missing_docs)]
-// #![cfg_attr(test, deny(warnings))]
+#![deny(missing_docs)]
+#![cfg_attr(test, deny(warnings))]
 #![feature(external_doc)]
 #![doc(include = "../README.md")]
 // #![cfg_attr(test, feature(plugin))]
