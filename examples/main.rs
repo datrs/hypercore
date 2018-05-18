38ea1cb8f529f6d5fcaeafe9f0ae556714439779
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-18 11:53:51 +0200

    add example

---
+++ examples/main.rs
@@ -0,0 +1,14 @@
+extern crate hypercore;
+
+use hypercore::Feed;
+use std::path::PathBuf;
+
+fn main () {
+  let mut feed = Feed::default();
+
+  feed.append(b"hello").unwrap();
+  feed.append(b"world").unwrap();
+
+  println!("{:?}", feed.get(0)); // prints "hello"
+  println!("{:?}", feed.get(1)); // prints "world"
+}
