51bb879a8500e7d4c26efe51684f52f6485bcaf2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-23 18:40:11 +0100

    update masks code

--- src/bitfield/masks.rs
+++ src/bitfield/masks.rs
@@ -5,13 +5,9 @@
 
 #[derive(Debug)]
 pub(super) struct Masks {
-  /// https://oeis.org/search?q=63+207+243+252&sort=&language=&go=Search
   pub index_update: Vec<u8>,
-  /// https://oeis.org/search?q=0+192+240+252&sort=&language=&go=Search
   pub index_iterate: Vec<u8>,
-  /// https://oeis.org/search?q=128%2C+192%2C+224%2C+240%2C+248%2C+252%2C+254%2C+255&sort=&language=&go=Search
   pub data_iterate: Vec<u8>,
-  /// https://oeis.org/A023689
   pub data_update: Vec<u8>,
   pub map_parent_right: Vec<u8>,
   pub map_parent_left: Vec<u8>,
@@ -20,12 +16,45 @@ pub(super) struct Masks {
   pub total_1_bits: Vec<u8>,
 }
 
+// Masks are underscored at every 8 bytes.
 impl Masks {
   pub fn new() -> Self {
-    let index_update = vec![63, 207, 243, 252];
-    let index_iterate = vec![0, 192, 240, 252];
-    let data_iterate = vec![128, 192, 224, 240, 248, 252, 254, 255];
-    let data_update = vec![127, 191, 223, 239, 247, 251, 253, 254];
+    let index_update = vec![
+      0b00111111, //  63
+      0b11001111, // 207
+      0b11110011, // 243
+      0b11111100, // 252
+    ];
+
+    let index_iterate = vec![
+      0b00000000, //   0
+      0b11000000, // 192
+      0b11110000, // 240
+      0b11111100, // 252
+    ];
+
+    let data_iterate = vec![
+      0b10000000, // 128
+      0b11000000, // 192
+      0b11100000, // 224
+      0b11110000, // 240
+      0b11111000, // 248
+      0b11111100, // 252
+      0b11111110, // 254
+      0b11111111, // 255
+    ];
+
+    let data_update = vec![
+      0b01111111, // 127
+      0b10111111, // 191
+      0b11011111, // 223
+      0b11101111, // 223
+      0b11110111, // 247
+      0b11111011, // 251
+      0b11111101, // 253
+      0b11111110, // 254
+    ];
+
     let mut map_parent_right = vec![0; 256];
     let mut map_parent_left = vec![0; 256];
     let mut next_data_0_bit = vec![0; 256];
