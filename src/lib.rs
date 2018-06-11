914e68b5413767978cc4d2c2d8fe3c8ffde229b1
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-06-11 13:24:44 +0200

    remove last instances of clippy

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(test, deny(warnings))]
 #![feature(external_doc)]
 #![doc(include = "../README.md")]
-// #![cfg_attr(test, feature(plugin))]
-// #![cfg_attr(test, plugin(clippy))]
 
 #[macro_use]
 extern crate lazy_static;
