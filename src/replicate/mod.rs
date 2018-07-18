a970ad7cf8e7c20459764a11faf8f1cfd505d1f6
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-07-18 13:32:38 +0200

    init Peer struct

--- src/replicate/mod.rs
+++ src/replicate/mod.rs
@@ -1,2 +1,20 @@
+use sparse_bitfield::Bitfield;
+
 /// A peer on the network.
-pub struct Peer {}
+// omitted fields: [
+//  feed,
+//  stream,
+//  inflightRequests,
+// ]
+pub struct Peer {
+  remote_id: usize,
+  remote_length: usize,
+  remote_bitfield: Bitfield,
+  remote_is_want: bool,
+  remote_is_downloading: bool,
+  is_live: bool,
+  is_sparse: bool,
+  is_downloading: bool,
+  is_uploading: bool,
+  max_requests: u16,
+}
