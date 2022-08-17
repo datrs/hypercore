0756a1d34ea3de13cf8c3aaa15807946a3fd6eb3
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2022-08-17 11:07:51 +0300

    Efficient API where the buffer is owned by the client

--- src/compact_encoding/mod.rs
+++ src/compact_encoding/mod.rs
@@ -9,18 +9,30 @@ pub struct State {
     pub start: usize,
     /// End position
     pub end: usize,
-    /// Buffer to hold the encoding
-    pub buffer: Vec<u8>,
 }
 
 impl State {
     /// Create emtpy state
     pub fn new() -> State {
-        State {
-            start: 0,
-            end: 0,
-            buffer: vec![],
-        }
+        State { start: 0, end: 0 }
+    }
+
+    /// Create a state with an already known size.
+    /// With this, you can/must skip the preencode step.
+    pub fn new_with_size(size: usize) -> (State, Box<[u8]>) {
+        (
+            State {
+                start: 0,
+                end: size,
+            },
+            vec![0; size].into_boxed_slice(),
+        )
+    }
+
+    /// After calling preencode(), this allocates the right size buffer to the heap.
+    /// Follow this with the same number of encode() steps to fill the created buffer.
+    pub fn create_buffer(&self) -> Box<[u8]> {
+        vec![0; self.end].into_boxed_slice()
     }
 }
 
@@ -30,23 +42,23 @@ where
     T: Debug,
 {
     /// Preencode
-    fn preencode(&mut self, value: T);
+    fn preencode(&mut self, value: &T);
 
-    // /// Encode
-    // fn encode(state: State, value: T);
+    /// Encode
+    fn encode(&mut self, value: &T, buffer: &mut Box<[u8]>);
 
     // /// Decode
     // fn decode(state: State) -> T;
 }
 
-impl CompactEncoding<String> for State {
-    fn preencode(&mut self, value: String) {
+impl CompactEncoding<&str> for State {
+    fn preencode(&mut self, value: &&str) {
         self.end += value.len();
     }
-}
 
-impl CompactEncoding<u64> for State {
-    fn preencode(&mut self, _value: u64) {
-        self.end += 8;
+    fn encode(&mut self, value: &&str, buffer: &mut Box<[u8]>) {
+        let len = value.len();
+        buffer[self.start..self.start + len].copy_from_slice(value.as_bytes());
+        self.start += len;
     }
 }
--- tests/compact_encoding.rs
+++ tests/compact_encoding.rs
@@ -1,13 +1,26 @@
 use hypercore::compact_encoding::{CompactEncoding, State};
 
 #[test]
-fn cenc_create() {
+fn cenc_create() -> std::io::Result<()> {
+    let str_value_1 = "foo";
+    let str_value_2 = "bar";
+
     let mut state = State::new();
     assert_eq!(state.start, 0);
     assert_eq!(state.end, 0);
-    assert_eq!(state.buffer.capacity(), 0);
-    state.preencode("test".to_string());
+    state.preencode(&str_value_1);
     assert_eq!(state.start, 0);
-    assert_eq!(state.end, 4);
-    assert_eq!(state.buffer.capacity(), 0);
+    assert_eq!(state.end, 3);
+    state.preencode(&str_value_2);
+    assert_eq!(state.start, 0);
+    assert_eq!(state.end, 6);
+    let mut buffer = state.create_buffer();
+    assert_eq!(buffer.len(), 6);
+    state.encode(&str_value_1, &mut buffer);
+    assert_eq!(state.start, 3);
+    assert_eq!(state.end, 6);
+    state.encode(&str_value_2, &mut buffer);
+    assert_eq!(state.start, 6);
+    assert_eq!(state.end, 6);
+    Ok(())
 }
