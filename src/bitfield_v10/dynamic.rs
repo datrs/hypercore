e448217319de95f18ec7c9813f3df94d807f597c
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2022-09-07 10:27:58 +0300

    Use intmap in place of big-sparse-array

--- src/bitfield_v10/dynamic.rs
+++ src/bitfield_v10/dynamic.rs
@@ -1,3 +1,17 @@
+use super::fixed::FixedBitfield;
+
+const DYNAMIC_BITFIELD_PAGE_SIZE: usize = 32768;
+
 /// Dynamic sized bitfield, uses a collection of `FixeddBitfield` elements.
 #[derive(Debug)]
-pub struct DynamicBitfield {}
+pub struct DynamicBitfield {
+    pages: intmap::IntMap<FixedBitfield>,
+}
+
+impl DynamicBitfield {
+    pub fn new() -> Self {
+        Self {
+            pages: intmap::IntMap::new(),
+        }
+    }
+}
