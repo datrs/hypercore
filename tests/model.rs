127df9ed89b39038537ccb69ba2891e8e21d3c9a
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-03-14 12:55:33 +0200

    Enabled quickcheck model test for v10 as well

--- tests/model.rs
+++ tests/model.rs
@@ -11,9 +11,19 @@ const MAX_FILE_SIZE: u64 = 5 * 10; // 5mb
 
 #[derive(Clone, Debug)]
 enum Op {
-    Get { index: u64 },
-    Append { data: Vec<u8> },
+    Get {
+        index: u64,
+    },
+    Append {
+        data: Vec<u8>,
+    },
+    #[cfg(feature = "v9")]
     Verify,
+    #[cfg(feature = "v10")]
+    Clear {
+        len_divisor_for_start: u8,
+        len_divisor_for_length: u8,
+    },
 }
 
 impl Arbitrary for Op {
@@ -32,14 +42,25 @@ impl Arbitrary for Op {
                 }
                 Op::Append { data }
             }
+            #[cfg(feature = "v9")]
             2 => Op::Verify,
+            #[cfg(feature = "v10")]
+            2 => {
+                let len_divisor_for_start: u8 = g.gen_range(1, 17);
+                let len_divisor_for_length: u8 = g.gen_range(1, 17);
+                Op::Clear {
+                    len_divisor_for_start,
+                    len_divisor_for_length,
+                }
+            }
             err => panic!("Invalid choice {}", err),
         }
     }
 }
 
-#[cfg(feature = "v9")]
 quickcheck! {
+
+  #[cfg(feature = "v9")]
   fn implementation_matches_model(ops: Vec<Op>) -> bool {
     async_std::task::block_on(async {
       let page_size = 50;
@@ -79,4 +100,83 @@ quickcheck! {
       true
     })
   }
+  #[cfg(all(feature = "v10", feature = "async-std"))]
+  fn implementation_matches_model(ops: Vec<Op>) -> bool {
+    async_std::task::block_on(assert_implementation_matches_model(ops))
+  }
+
+  #[cfg(all(feature = "v10", feature = "tokio"))]
+  fn implementation_matches_model(ops: Vec<Op>) -> bool {
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(async {
+      assert_implementation_matches_model(ops).await
+    })
+  }
+}
+
+#[cfg(feature = "v10")]
+async fn assert_implementation_matches_model(ops: Vec<Op>) -> bool {
+    use hypercore::{Hypercore, Storage};
+
+    let storage = Storage::new_memory()
+        .await
+        .expect("Memory storage creation should be successful");
+    let mut hypercore = Hypercore::new(storage)
+        .await
+        .expect("Hypercore creation should be successful");
+
+    let mut model: Vec<Option<Vec<u8>>> = vec![];
+
+    for op in ops {
+        match op {
+            Op::Append { data } => {
+                hypercore
+                    .append(&data)
+                    .await
+                    .expect("Append should be successful");
+                model.push(Some(data));
+            }
+            Op::Get { index } => {
+                let data = hypercore
+                    .get(index)
+                    .await
+                    .expect("Get should be successful");
+                if index >= hypercore.info().length {
+                    assert_eq!(data, None);
+                } else {
+                    assert_eq!(data, model[index as usize].clone());
+                }
+            }
+            Op::Clear {
+                len_divisor_for_start,
+                len_divisor_for_length,
+            } => {
+                let start = {
+                    let result = model.len() as u64 / len_divisor_for_start as u64;
+                    if result == model.len() as u64 {
+                        if model.len() > 0 {
+                            result - 1
+                        } else {
+                            0
+                        }
+                    } else {
+                        result
+                    }
+                };
+                let length = model.len() as u64 / len_divisor_for_length as u64;
+                let end = start + length;
+                let model_end = if end < model.len() as u64 {
+                    end
+                } else {
+                    model.len() as u64
+                };
+                hypercore
+                    .clear(start, end)
+                    .await
+                    .expect("Clear should be successful");
+                model[start as usize..model_end as usize].fill(None);
+            }
+        }
+    }
+    true
 }
