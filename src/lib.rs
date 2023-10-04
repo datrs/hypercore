4ca4037661ebb85ccbacccd04adf82d86e4ff573
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-10-04 12:42:35 +0300

    Add basic library level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,70 @@
 #![doc(test(attr(deny(warnings))))]
 
 //! ## Introduction
+//!
 //! Hypercore is a secure, distributed append-only log. Built for sharing
 //! large datasets and streams of real time data as part of the [Dat] project.
-//! This is a rust port of [the original node version][dat-node]
-//! aiming for interoperability. The primary way to use this crate is through the [Feed] struct.
+//! This is a rust port of [the original Javascript version][holepunch-hypercore]
+//! aiming for interoperability with LTS version. The primary way to use this
+//! crate is through the [Hypercore] struct, which can be created using the
+//! [HypercoreBuilder].
+//!
+//! This crate supports WASM with `cargo build --target=wasm32-unknown-unknown`.
+//!
+//! ## Features
+//!
+//! ### `sparse` (default)
+//!
+//! When using disk storage, clearing values may create sparse files. On by default.
+//!
+//! ### `async-std` (default)
+//!
+//! Use the async-std runtime, on by default. Either this or `tokio` is mandatory.
+//!
+//! ### `tokio`
+//!
+//! Use the tokio runtime. Either this or `async_std` is mandatory.
+//!
+//! ### `cache`
+//!
+//! Use a moka cache for merkle tree nodes to speed-up reading.
 //!
 //! ## Example
 //! ```rust
-//! # fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//! # #[cfg(feature = "tokio")]
+//! # tokio_test::block_on(async {
+//! # example().await;
+//! # });
+//! # #[cfg(feature = "async-std")]
 //! # async_std::task::block_on(async {
-//! // unimplemented
-//! Ok(())
-//! # })
+//! # example().await;
+//! # });
+//! # async fn example() {
+//! use hypercore::{HypercoreBuilder, Storage};
+//!
+//! // Create an in-memory hypercore using a builder
+//! let mut hypercore = HypercoreBuilder::new(Storage::new_memory().await.unwrap())
+//!     .build()
+//!     .await
+//!     .unwrap();
+//!
+//! // Append entries to the log
+//! hypercore.append(b"Hello, ").await.unwrap();
+//! hypercore.append(b"world!").await.unwrap();
+//!
+//! // Read entries from the log
+//! assert_eq!(hypercore.get(0).await.unwrap().unwrap(), b"Hello, ");
+//! assert_eq!(hypercore.get(1).await.unwrap().unwrap(), b"world!");
 //! # }
 //! ```
 //!
-//! [dat-node]: https://github.com/mafintosh/hypercore
+//! Find more examples in the [examples] folder.
+//!
 //! [Dat]: https://github.com/datrs
-//! [Feed]: crate::feed::Feed
+//! [holepunch-hypercore]: https://github.com/holepunchto/hypercore
+//! [Hypercore]: crate::core::Hypercore
+//! [HypercoreBuilder]: crate::builder::HypercoreBuilder
+//! [examples]: https://github.com/datrs/hypercore/tree/master/examples
 
 pub mod encoding;
 pub mod prelude;
